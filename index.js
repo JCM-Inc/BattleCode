dcf6fb445d5502d746b60fce84b7eb32c5bed3d6
Jmetev1 <jmetevier@gmail.com>
2017-08-01 14:32:40 -0500

    small server updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require('path');
 // const util = require('./util')
@@ -7,15 +7,13 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/client')));
-
-dotenv.load();
-app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(bodyParser.json());
-app.listen(app.get('port'));
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', (req, res) => {
   res.end('serving battleCODES!');
 });
+app.listen(app.get('port'));
