1aef0f5180e2b94c8b7d297645e61300bb86fd90
Jmetev1 <jmetevier@gmail.com>
2017-08-01 21:07:42 -0500

    [add] schemas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const path = require('path');
-// const db = require('./dbTools'); 
+const db = require('./dbTools'); 
 
 const app = express();
 
@@ -13,4 +13,4 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'),() => console.log('listening on port', app.get('port')));
+app.listen(app.get('port'), () => console.log('listening on port', app.get('port')));
