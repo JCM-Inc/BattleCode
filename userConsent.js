38082821c698d474b674cd4c2371170daac01a74
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-02 12:16:41 -0500

    [layout] auth

--- userConsent.js
+++ userConsent.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const plus = google.plus('v1');
 const OAuth2 = google.auth.OAuth2;
+
+
 const oauth2Client = new OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
@@ -23,3 +25,28 @@ plus.people.get({
 }, (err, user) => {
   console.log(`Result: ${err ? err.message : user.displayName}`);
 });
+
+// function handleClientLoad() {
+//   // Loads the client library and the auth2 library together for efficiency.
+//   // Loading the auth2 library is optional here since `gapi.client.init` function will load
+//   // it if not already loaded. Loading it upfront can save one network request.
+//   gapi.load('client:auth2', initClient);
+// }
+
+// function initClient() {
+//   // Initialize the client with API key and People API, and initialize OAuth with an
+//   // OAuth 2.0 client ID and scopes (space delimited string) to request access.
+//   gapi.client.init({
+//     apiKey: 'YOUR_API_KEY',
+//     discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+//     clientId: 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+//     scope: 'profile'
+//   }).then(function () {
+//     // Listen for sign-in state changes.
+//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+//     // Handle the initial sign-in state.
+//     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//   });
+// }
+// f
