8ba00c1f2e28713e4595ec2f4eeab7b4dd4cb4d3
Cain <cainwatson@users.noreply.github.com>
2017-08-02 19:35:44 -0500

    (add) mocha test functionality

---
+++ public/js/React Components/Competition/Test.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log('done')
+    mocha.setup('bdd');
+    eval(`
+      const expect = chai.expect;
+      describe('test', () => {
+        it('testtest', () => {
+          expect(2).to.equal(2);
+        })
+      })
+    `);
+    mocha.run();
+  }
+
+  render() {
+    return (
+      <div id="mocha" />
+    );
+  }
+}
