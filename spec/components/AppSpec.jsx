06a1912008bf3a132945a904130ba9e63d7f3d85
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-08-10 17:52:15 -0500

    [add] layout for unit testing

---
+++ spec/components/AppSpec.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+(function () {
+  describe('App', () => {
+    describe('maybe a bit more context here', function () {
+      it('should run here few assertions', function () {
+        expect(true).toBe(true);
+      });
+    });
+  });
+})();
