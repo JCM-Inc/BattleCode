e3b2097994d8a9fcfb1b51e7e7839653e9475f94
Jmetev1 <jmetevier@gmail.com>
2017-08-11 10:59:11 -0500

    [remove] consolelogs

--- public/js/React Components/Signin/Signin.jsx
+++ public/js/React Components/Signin/Signin.jsx
@@ -26,10 +26,7 @@ export default class Signin extends Component {
     const idToken = googleUser.getAuthResponse().id_token;
     const profile = googleUser.getBasicProfile();
     const userEmail = profile.getEmail();
-<<<<<<< HEAD
-=======
     console.log({ accessToken: idToken, email: userEmail });
->>>>>>> [add] redirect to dashboard
 
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/signin', true);
