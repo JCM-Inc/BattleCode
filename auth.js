8ca1f322a89e84600e625c41a067dea0c06a6fe4
awinste <awinste@gmail.com>
2018-01-03 13:13:49 -0600

    allow login through google, need to add redirect urls

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const GoogleAuth = require('google-auth-library');
 
 const auth = new GoogleAuth;
-const CLIENT_ID = '106454631553-mles8i7ktt96qbvps7uoh2k9idop90e0.apps.googleusercontent.com';
+const CLIENT_ID = '500724803395-pma9je3unef7t0ugokspsjq3vq9olu7d.apps.googleusercontent.com'
 const client = new auth.OAuth2(CLIENT_ID);
 exports.tokenCheck = (token, cb) => {
   client.verifyIdToken(
