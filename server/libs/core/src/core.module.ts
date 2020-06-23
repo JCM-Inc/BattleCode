7e0ecc455e97fec5af508b1d9e5e6707466ac3f5
Cain Watson <cainwatson00@gmail.com>
2020-06-23 04:55:29 -0500

    feat: install typeorm

--- server/libs/core/src/core.module.ts
+++ server/libs/core/src/core.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoreService } from './core.service';
 
 @Module({
   providers: [CoreService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: '',
+      database: 'battlecode_dev',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   exports: [CoreService],
 })
 export class CoreModule {}
