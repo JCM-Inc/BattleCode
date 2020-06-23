7f0dbeb7b1af8aeff87220064677a3dc686b85e4
Cain Watson <cainwatson00@gmail.com>
2020-06-23 05:25:09 -0500

    feat: install nest config

--- server/apps/web/src/web.module.ts
+++ server/apps/web/src/web.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CoreService } from 'core/core';
 import { WebController } from './web.controller';
 import { WebService } from './web.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [WebController],
-  providers: [WebService],
+  providers: [CoreService, WebService],
 })
 export class WebModule {}
