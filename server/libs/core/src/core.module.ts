9a4d6112d28665bbf83b2a85fe9a434ff479ea7e
Cain Watson <cainwatson00@gmail.com>
2020-06-23 05:25:09 -0500

    feat: install nest config

--- server/libs/core/src/core.module.ts
+++ server/libs/core/src/core.module.ts
@@ -7,11 +7,11 @@ import { CoreService } from './core.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: '',
-      database: 'battlecode_dev',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number.parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || 'battlecode_dev',
       entities: [],
       synchronize: true,
     }),
